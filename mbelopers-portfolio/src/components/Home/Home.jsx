98cc5ed5cd4de9d92947cfb58f4efe3b3063454c
MBarcilay <mariobarcilay@gmail.com>
2022-11-02 20:46:39 -0600

    Nav and about update
    The nav menu is now interactive with react-router-dom and the about page is finished.

--- mbelopers-portfolio/src/components/Home/Home.jsx
+++ mbelopers-portfolio/src/components/Home/Home.jsx
@@ -4,14 +4,9 @@ import './Home.css'
 
 import MBelopersLogo from '../../assets/MBeloper-logo sin fondo.png'
 
-import Nav from '../Shared/Nav/Nav'
-import Footer from '../Shared/Footer/Footer'
-
 const Home = () => {
   return (
     <div className='home-div'>
-
-        <Nav></Nav>
 
         <main className='home-content'>
 
@@ -22,15 +17,14 @@ const Home = () => {
             <section className='resume-section'>
                 <h1 className='mbelopers-title'>MBeloper</h1>
                 <p className='info-p'>
-                    Hola, mi nombre es Mario Barcilay, soy un desarrollador de software mexicano y creador de contenido digital, en busca de seguir aprendiendo.
+                    Hola, mi nombre es Mario Barcilay, soy un desarrollador web mexicano y creador de contenido digital, en busca de seguir aprendiendo.
+                    <br />
                     <br />
                     Aquí podrás encontrar información sobre mi, mis proyectos y mis conocimientos, así como contactar conmigo.
                 </p>
             </section>
 
         </main>
-
-        <Footer></Footer>
 
     </div>
   )
