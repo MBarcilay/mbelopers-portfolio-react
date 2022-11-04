eeabf81eb1cd70ad5fe74429a268262adca08113
MBarcilay <mariobarcilay@gmail.com>
2022-11-04 00:08:41 -0600

    Nav and Knowledge page update 2
    The nav now shows which is the active page and the knowledge page style is finished.

--- mbelopers-portfolio/src/components/Knowledge/Knowledge.jsx
+++ mbelopers-portfolio/src/components/Knowledge/Knowledge.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+import QR_Code from '../../assets/Certifications_QR.png'
+import GithubLogo from '../../assets/github.png'
+import HTML_Logo from '../../assets/html-5.png'
+import CSS_Logo from '../../assets/css-3.png'
+import ReactLogo from '../../assets/React-icon.svg.png'
+import JavaScriptLogo from '../../assets/javascript-logo.png'
+import NodeJS_Logo from '../../assets/nodejs-logo.png'
+import FlutterLogo from '../../assets/flutter-logo.png'
+
 import './Knowledge.css'
 
 const Knowledge = () => {
@@ -10,72 +19,122 @@ const Knowledge = () => {
 
         <section className='knowledge-info-section'>
           <p className='knowledge-info-p'>
-            Poseo varias certiificaciones en tecnologías de desarrollo web, de escritorio y móvil, aunque mi especialidad es el desarrollo web.
+            Poseo varias <b>certiificaciones</b> en tecnologías de desarrollo web, de escritorio y móvil, aunque mi especialidad es el <b>desarrollo web</b>.
             <br />
             <br />
-            Además, tengo distintas certificaciones en tecnologías del área, estas las puedes revisar escaneando el siguiente código QR.
+            Además, tengo distintas certificaciones en tecnologías del área, estas las puedes revisar escaneando el siguiente <b>código QR</b>.
           </p>
         </section>
 
         <section className='QR-section'>
-          <img src="" alt="Código QR de mi portafolio" />
+          <img src={QR_Code} alt="Código QR de mi portafolio" />
         </section>
 
         <section className='technologies-section'>
 
           <div className='technologies-item'>
 
-            <h3 className='technology-name'></h3>
+            <h3 className='technology-name'>HTML 5</h3>
 
-            <img src="" alt="HTML 5 Logo" />
-
-          </div>
-
-          <div className='technologies-item'>
-
-            <h3 className='technology-name'></h3>
-
-            <img src="" alt="CSS 3 Logo" />
+            <img src={HTML_Logo} alt="HTML Logo" />
 
           </div>
 
+
           <div className='technologies-item'>
 
-            <h3 className='technology-name'></h3>
+            <h3 className='technology-name'>CSS 3</h3>
 
-            <img src="" alt="JavaScript ES9 Logo" />
+            <img src={CSS_Logo} alt="CSS Logo" />
 
           </div>
 
+
           <div className='technologies-item'>
 
-            <h3 className='technology-name'></h3>
+            <h3 className='technology-name'>JavaScript ES9</h3>
+
+            <img src={JavaScriptLogo} alt="JavaScript Logo" />
+
+          </div>
+
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'>React</h3>
+
+            <img src={ReactLogo} alt="React Logo" />
+
+          </div>
+
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'>Node JS</h3>
+
+            <img src={NodeJS_Logo} alt="Node JS Logo" />
+
+          </div>
+
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'>Flutter</h3>
+
+            <img src={FlutterLogo} alt="Flutter Logo" />
+
+          </div>
+
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'>C#</h3>
+
+            <img src="" alt="C# Logo" />
+
+          </div>
+
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'>Python</h3>
 
             <img src="" alt="Python Logo"/>
 
           </div>
 
+
           <div className='technologies-item'>
 
-            <h3 className='technology-name'></h3>
+            <h3 className='technology-name'>MySQL</h3>
 
             <img src="" alt="MySQL Logo" />
 
           </div>
 
+
           <div className='technologies-item'>
 
-            <h3 className='technology-name'></h3>
+            <h3 className='technology-name'>Mongo DB</h3>
 
             <img src="" alt="Mongo DB Logo" />
 
           </div>
 
+
           <div className='technologies-item'>
 
-            <h3 className='technology-name'></h3>
+            <h3 className='technology-name'>Pandas</h3>
 
             <img src="" alt="Pandas Logo" />
+
+          </div>
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'>GitHub</h3>
+
+            <img src={GithubLogo} alt="Github Logo" />
 
           </div>
 
