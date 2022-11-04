430323dffbf7788e1f220e671a9bd73928e5bd83
MBarcilay <mariobarcilay@gmail.com>
2022-11-03 20:49:53 -0600

    Knowledge and 404 update
    The page for 404 error is now defined and also de knowledge page structure.

--- mbelopers-portfolio/src/components/Knowledge/Knowledge.jsx
+++ mbelopers-portfolio/src/components/Knowledge/Knowledge.jsx
@@ -10,11 +10,74 @@ const Knowledge = () => {
 
         <section className='knowledge-info-section'>
           <p className='knowledge-info-p'>
-            
+            Poseo varias certiificaciones en tecnologías de desarrollo web, de escritorio y móvil, aunque mi especialidad es el desarrollo web.
+            <br />
+            <br />
+            Además, tengo distintas certificaciones en tecnologías del área, estas las puedes revisar escaneando el siguiente código QR.
           </p>
         </section>
 
+        <section className='QR-section'>
+          <img src="" alt="Código QR de mi portafolio" />
+        </section>
+
         <section className='technologies-section'>
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'></h3>
+
+            <img src="" alt="HTML 5 Logo" />
+
+          </div>
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'></h3>
+
+            <img src="" alt="CSS 3 Logo" />
+
+          </div>
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'></h3>
+
+            <img src="" alt="JavaScript ES9 Logo" />
+
+          </div>
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'></h3>
+
+            <img src="" alt="Python Logo"/>
+
+          </div>
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'></h3>
+
+            <img src="" alt="MySQL Logo" />
+
+          </div>
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'></h3>
+
+            <img src="" alt="Mongo DB Logo" />
+
+          </div>
+
+          <div className='technologies-item'>
+
+            <h3 className='technology-name'></h3>
+
+            <img src="" alt="Pandas Logo" />
+
+          </div>
 
         </section>
 
